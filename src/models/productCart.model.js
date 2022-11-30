b84935a1feac7490e7703b8c343547be7c8eb83b
SabahatSyed <sabahatsyed2000@gmail.com>
2022-11-30 15:02:10 +0500

    changes

--- src/models/productCart.model.js
+++ src/models/productCart.model.js
@@ -5,6 +5,7 @@ const Id = mongoose.Schema.Types.ObjectId;
 const ProductCartSchema = new mongoose.Schema({
   user_id: { type: Id, ref: "User", required: true },
   status: { type: String, default: "Pending" },
+  Total:{type:Number},
   products: [
     {
       product_id: { type: Id, ref: "Product" },
